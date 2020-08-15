a1308618317adc8c0404e81c13b7649ec15f47ce
william <willpickeral@gmail.com>
2020-08-15 16:14:57 -0400

    updated readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='make_sass_files',
-    version='1.1.0',
+    version='1.1.1',
     author="William L. Pickeral",
     author_email="willpickeral@gmail.com",
     description="A simple program to implement Sass 7-1 or 3-1 architecture",
